c0c11e65a91a81e455e6e70415e706874ab15719
amitshah588 <anticamit@gmail.com>
2021-10-26 23:16:26 +0530

    app and functioal

---
+++ src/Functionalcomp.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Functionalcomp(){
+    return(
+        <div>
+            <h1>This is my 1st functional component App</h1>
+            <button>Yes</button>
+            <button>No</button>
+        </div>
+
+    )
+}
+
+export default Functionalcomp;
